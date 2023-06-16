2319155b27b789770dd78f8ece60dd6c42eef8a1
ylose <heungeob1003@gmail.com>
2023-06-17 01:50:08 +0900

    import assets
    import assets

---
+++ Assets/VRShooting/Scripts/Example.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Example : MonoBehaviour {
+
+    // Use this for initialization
+    void Start()
+    {
+        // 처음에 한번 메시지를 표시한다
+        Debug.Log("[Start]");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Space 키가 눌려 있는 동안 메시지를 표시한다
+        if (Input.GetKey(KeyCode.Space))
+        {
+            Debug.Log("[Update] Space key pressed");
+        }
+    }
+}
