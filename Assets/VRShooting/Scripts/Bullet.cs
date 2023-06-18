8e85018a77a2bf6cf84d81b7c54c7089c5ed4256
ylose <heungeob1003@gmail.com>
2023-06-19 02:14:55 +0900

    create Bullet C# Script
    create Bullet C# Script

---
+++ Assets/VRShooting/Scripts/Bullet.cs
@@ -0,0 +1,22 @@
+ using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class Bullet : MonoBehaviour
+{
+    [SerializeField] float speed = 20f; // 총알 속도 [m/s]
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //게임 오브젝트 앞쪽 방향의 속도 벡터를 계산
+        var velocity = speed * transform.forward;
+
+        // Rigidbody 컴포넌트를 취득
+        var rigidbody = GetComponent<rigidbody>();
+
+        // Rigidbody 컴포넌트를 사용해 시작 속도를 준다
+        rigidbody.AddForce(velocity, ForceMode.VelocityChange);
+    }
+}
