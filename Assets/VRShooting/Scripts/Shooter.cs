6b672dbbb506473307bd16117552e038dff47ce6
ylose <heungeob1003@gmail.com>
2023-06-19 01:59:42 +0900

    add Shooter C# Script
    add Shooter C# Script

--- Assets/VRShooting/Scripts/Shooter.cs
+++ Assets/VRShooting/Scripts/Shooter.cs
@@ -4,15 +4,22 @@ using UnityEngine;
 
 public class Shotter : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] GameObject bulletPrefab; // 총알 프리팹
+    [SerializeField] Transform gunBarrelEnd; // 총구(총알의 발사 위치)
 
     // Update is called once per frame
     void Update()
     {
-        
+        // 입력에 따라 총알을 발사한다.
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Shoot();
+        }
+
+        void Shoot()
+        {
+            // 프리팹을 바탕으로 씬상에 총알을 생성
+            Instantiate(bulletPrefab, gunBarrelEnd.position, gunBarrelEnd.rotation);
+        }
     }
 }
