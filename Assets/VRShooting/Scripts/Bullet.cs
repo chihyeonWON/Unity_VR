90d9085518aa4748f4d40f86bc09c223bd432b84
ylose <heungeob1003@gmail.com>
2023-06-20 02:16:54 +0900

    add OnTriggerEnter Method to Bullet C# Script
    add OnTriggerEnter Method to Bullet C# Script

--- Assets/VRShooting/Scripts/Bullet.cs
+++ Assets/VRShooting/Scripts/Bullet.cs
@@ -19,4 +19,14 @@ public class Bullet : MonoBehaviour
         // Rigidbody 컴포넌트를 사용해 시작 속도를 준다
         rigidbody.AddForce(velocity, ForceMode.VelocityChange);
     }
+
+    // 트리거 영역 진입 시에 호출된다.
+    void onTriggerEnter(Collider other)
+    {
+        // 충돌 대상에 "OnHitBullet" 메시지
+        other.SendMessage("OnHitBullet");
+
+        // 자신의 게임 오브젝트를 제거
+        Destroy(gameObject);
+    }
 }
