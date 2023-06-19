a1827e8d81baf205c69a5a22c3558ff0f996dc46
ylose <heungeob1003@gmail.com>
2023-06-20 03:02:13 +0900

    modify Method name onHitMessage to onHitBullet
    modify Method name onHitMessage to onHitBullet

--- Assets/VRShooting/Scripts/Enemy.cs
+++ Assets/VRShooting/Scripts/Enemy.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class Enemy : MonoBehaviour
 {
     // OnHitMessage 메시지로부터 호출되는 것을 상정
-    void OnHitMessage()
+    void OnHitBullet()
     {
         // 자신의 게임 오브젝트를 제거
         Destroy(gameObject);
