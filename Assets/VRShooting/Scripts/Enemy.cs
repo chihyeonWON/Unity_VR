4a9578aea49b7908b8ce60682d26dcbc6726719f
ylose <heungeob1003@gmail.com>
2023-06-25 13:49:16 +0900

    modify Enemy Script to adding Score system
    modify Enemy Script to adding Score system

--- Assets/VRShooting/Scripts/Enemy.cs
+++ Assets/VRShooting/Scripts/Enemy.cs
@@ -14,13 +14,22 @@ public class Enemy : MonoBehaviour
 
     AudioSource audioSource; // 재생에 사용하는 AudioSource
 
+    [SerializeField] int point = 1;
+    Score score;
+
     void Start()
     {
         // AudioSource 컴포넌트를 취득해 둔다.
         audioSource = GetComponent<AudioSource>();
 
         // 출현 시의 소리를 재생
-        audioSource.PlayOneShot(spawnClip); 
+        audioSource.PlayOneShot(spawnClip);
+
+        // 게임 오브젝트를 검색
+        var gameObj = GameObject.FindWithTag("Score");
+
+        // gameObj에 포함되는 Score 컴포넌트를 취득
+        score = gameObj.GetComponent<Score>();
     }
 
     // OnHitMessage 메시지로부터 호출되는 것을 상정
@@ -35,6 +44,9 @@ public class Enemy : MonoBehaviour
 
     void GoDown()
     {
+        // 점수를 더함
+        score.AddScore(point);
+
         // 충돌 판정과 표시를 지운다
         enemyCollider.enabled = false;
 
