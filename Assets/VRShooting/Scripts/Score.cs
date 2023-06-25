17abddbab11a902272a792c5bb1ea320a33f19ce
ylose <heungeob1003@gmail.com>
2023-06-25 13:41:54 +0900

    add Score C# Script
    add Score C# Script which is printing current score

---
+++ Assets/VRShooting/Scripts/Score.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Text))]
+public class Score : MonoBehaviour
+{
+    Text uiText; // UIText 컴포넌트
+
+    public int Points { get; private set; } // 현재의 점수 포인트
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        uiText = GetComponent<Text>();
+    }
+
+    public void AddScore(int addPoint)
+    {
+        // 현재의 포인트에 더함
+        Points += addPoint;
+
+        // 점수 갱신
+        uiText.text = string.Format("점수:{0:D3}점", Points);
+    }
+}
+
