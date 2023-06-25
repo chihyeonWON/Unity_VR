a7b1b3248ab18c395e38973ed6069cde6f9340a6
ylose <heungeob1003@gmail.com>
2023-06-26 02:02:58 +0900

    add Result Score C# Script
    add Result Score C# Script

---
+++ Assets/VRShooting/Scripts/ResultScore.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Text))]
+public class ResultScore : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        // 게임 오브젝트를 검색
+        var gameObj = GameObject.FindWithTag("Score");
+
+        // gameObj에 포함되는 Score 컴포넌트를 취득
+        var score = gameObj.GetComponent<Score>();
+
+        // Text 컴포넌트의 취득
+        var uiText = GetComponent<Text>();
+
+        // 점수 갱신
+        uiText.text = string.Format("점수 : {0:D3}점", score.Points);
+    }
+}
