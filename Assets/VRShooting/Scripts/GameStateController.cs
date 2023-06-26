54d82d3a3d73433a1e771785dd57c07cf6fe1e29
ylose <heungeob1003@gmail.com>
2023-06-27 01:02:21 +0900

    add ReadyState class
    add ReadyState class

--- Assets/VRShooting/Scripts/GameStateController.cs
+++ Assets/VRShooting/Scripts/GameStateController.cs
@@ -30,4 +30,31 @@ public class GameStateController : MonoBehaviour
         public virtual void onExit() { }
     }
 
+    // 게임 시작 준비 스테이트
+    class ReadyState : BaseState
+    {
+        float timer;
+
+        public ReadyState(GameStateController c) : base(c) { }
+        public override void OnEnter()
+        {
+            // ready 문자열을 표시
+            Controller.gameReady.SetActive(true);
+        }
+        public override StateAction OnUpdate()
+        {
+            timer += Time.deltaTime;
+            // 5초 후에 다음으로
+            if(timer > 5.0f)
+            {
+                return StateAction.STATE_ACTION_NEXT;
+            }
+            return StateAction.STATE_ACTION_WAIT;
+        }
+        public override void onExit()
+        {
+            // ready 문자열을 숨김
+            Controller.gameReady.SetActive(false);
+        }
+    }
 }
