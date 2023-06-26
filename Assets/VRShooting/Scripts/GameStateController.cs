6a8946c9e89ac5527cce45d3a69ce984926f5a26
ylose <heungeob1003@gmail.com>
2023-06-27 00:59:37 +0900

    Define abstract class BaseState
    Define abstract class BaseState

--- Assets/VRShooting/Scripts/GameStateController.cs
+++ Assets/VRShooting/Scripts/GameStateController.cs
@@ -11,4 +11,23 @@ public class GameStateController : MonoBehaviour
     [SerializeField] GameObject result; // Result 게임 오브젝트 참조
     [SerializeField] GameObject player; // PlayerGun 게임 오브젝트 참조
     [SerializeField] GameObject spawners; // Spawner 게임 오브젝트 참조
+
+    // 스테이트 베이스 클래스
+    abstract class BaseState
+    {
+        public GameStateController Controller { get; set; }
+
+        public enum StateAction
+        {
+            STATE_ACTION_WAIT,
+            STATE_ACTION_NEXT
+        }
+
+        public BaseState(GameStateController c) { Controller = c; }
+
+        public virtual void OnEnter() { }
+        public virtual StateAction OnUpdate() { return StateAction.STATE_ACTION_NEXT; }
+        public virtual void onExit() { }
+    }
+
 }
